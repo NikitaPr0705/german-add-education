a861b6680dc32c7f8e26e142782f2230f5e9d66f
NikitPr0705 <iamnik0705@gmail.com>
2020-10-16 13:05:43 +0300

    pre-final commit

--- Школа доп образования/src/js/script.js
+++ Школа доп образования/src/js/script.js
@@ -94,9 +94,10 @@ btn.onclick = function() {
 
 
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target === modal) {
         modal.style.display = "none";
         modalCnt.style.top = "-125vw";
 
     }
 }
+
